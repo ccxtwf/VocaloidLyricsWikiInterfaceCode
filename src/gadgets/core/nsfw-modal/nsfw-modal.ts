597d05b5af3ceaa65fc34db0b3309a170fe5f3ef
ccxtwf <theweirdfandom@gmail.com>
2026-05-27 21:34:10 +0700

    nsfw-modal: Refactor to use Vue + Codex

--- src/gadgets/core/nsfw-modal/nsfw-modal.ts
+++ src/gadgets/core/nsfw-modal/nsfw-modal.ts
@@ -1,5 +1,8 @@
+/// <reference types="vue" />
 /// <reference types="types-mediawiki" />
 /// <reference types="types-mediawiki-api" />
+
+import type { Reactive, App } from "vue";
 
 "use strict";
 
@@ -17,7 +20,12 @@ const USER_PREFERENCE_EXPIRY_KEY = USER_PREFERENCE_KEY+'-EXPIRY';
 const ONE_DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
 const SUPPRESS_EXPIRY = 30 * ONE_DAY_IN_MILLISECONDS;	 // 1 month
 let skipIds: string[] = [];
-let $modal: JQuery<HTMLElement>;
+
+const container = document.createElement('div');
+container.setAttribute('id', 'cw-modal');
+container.style.display = 'none';
+
+let store: Reactive<IAppStore>;
 
 const messages = {
 	'vlw-nsfw-modal--cw-heading': 'The following content is not safe for work.',
@@ -27,6 +35,10 @@ const messages = {
 	'vlw-nsfw-modal--do-not-show': 'Don\'t show me any more of these warnings.',
 };
 mw.messages.set(messages);
+
+interface IAppStore {
+  doNotShowMoreWarnings: boolean,
+}
 
 /**
  * Checks if the user has set preferences to disable the site-wide NSFW modals
@@ -72,8 +84,8 @@ async function saveUserOptions(): Promise<void> {
     const expiry = new Date(Date.now() + SUPPRESS_EXPIRY).getTime();
     const api = new mw.Api();
     await api.saveOptions( {
-      USER_PREFERENCE_KEY: '1',
-      USER_PREFERENCE_EXPIRY_KEY: ''+expiry,
+      [USER_PREFERENCE_KEY]: '1',
+      [USER_PREFERENCE_EXPIRY_KEY]: ''+expiry,
     } );
   } else {
     /**
@@ -83,7 +95,14 @@ async function saveUserOptions(): Promise<void> {
   }
 }
 
-(function (mw, $) {
+/**
+ * Utility function for UI update
+ */
+function showOrHideModal(isShown: boolean) {
+  container.style.display = isShown ? '' : 'none';
+}
+
+(function (mw) {
   /**
    * Skip if namespace is not main or the category is not detected
    */
@@ -113,91 +132,95 @@ async function saveUserOptions(): Promise<void> {
     return;
   }
 
-  /**
-   * Event handler on clicking the "Yes, I'm over 18" Button
-   */
-	function onClickConfirm(): void {
-		$('#cw-modal').hide();
-
-		// Save user consent to session storage
-		skipIds.push(''+config.wgArticleId);
-		mw.cookie.set( SUPPRESSED_PAGE_IDS_KEY, skipIds.join(',') );
-
-		// Save option to user preferences
-		if ($('#cw-suppress-nsfw-notifs').prop('checked') === true) {
-      saveUserOptions();  //! unawaited
-		}
-
-		// Clear scroll locking
-		const scrollY = document.body.style.top;
-		document.body.style.position = '';
-		document.body.style.top = '';
-		window.scrollTo(0, parseInt(scrollY || '0') * -1);
-	}
-
-  /**
-   * Event handler on clicking the "Let me go back" Button
-   */
-	function onClickBack() {
-		if (window.history.length > 1) {
-			window.history.back();
-		}
-		window.close();
-		// Fallback: If window.close() doesn't work (blocked by browser),
-		// redirect to a blank page
-		setTimeout(() => {
-			if (!window.closed) {
-				window.location.href = 'about:blank';
-			}
-		}, 100);
-	}
-
 	function init() {
-		$modal = $('<div>', { id: 'cw-modal' })
-			.append(
-				$('<div>', { id: 'cw-modal-box' })
-					.append(
-						$('<div>', { id: 'cw-modal-content' })
-							.append(
-								$('<div>', { id: 'cw-heading' })
-									.text(
-										mw.msg('vlw-nsfw-modal--cw-heading')
-									),
-                $('<div>', { id: 'cw-subheading' })
-									.text(
-										mw.msg('vlw-nsfw-modal--cw-subheading')
-									),
-                $('<div>', { id: 'cw-action-buttons' })
-									.append(
-										$('<button>', { id: 'cw-confirm', type: 'button', 'class': 'cw-action-button' })
-											.text(mw.msg('vlw-nsfw-modal--cw-confirm')),
-										$('<button>', { id: 'cw-back', type: 'button', 'class': 'cw-action-button' })
-											.text(mw.msg('vlw-nsfw-modal--cw-back'))
-									),
-								$('<div>', { id: 'cw-more-actions' })
-									.append(
-										$('<label>')
-											.append(
-                        $('<input>', { id: 'cw-suppress-nsfw-notifs', type: 'checkbox' }),
-												$('<span>').text(
-													mw.msg('vlw-nsfw-modal--do-not-show')
-												)
-											)
-									)
-							)
-					)
-			);
-		$('body').append($modal);
-		$('#cw-modal #cw-confirm').on('click', onClickConfirm);
-		$('#cw-modal #cw-back').on('click', onClickBack);
+    mw.loader.using( ['vue', '@wikimedia/codex'] ).then(require => {
+			const Vue = require('vue');
+			const { CdxCheckbox } = require('@wikimedia/codex');
+
+      store = Vue.reactive({
+				doNotShowMoreWarnings: false,
+			});
+
+      const $app: App = Vue.createMwApp({
+        template: `
+        <div id="cw-modal-box-wrapper">
+          <div id="cw-modal-box">
+            <div id="cw-heading">
+              {{ $i18n( 'vlw-nsfw-modal--cw-heading' ).text() }}
+            </div>
+            <div id="cw-subheading">
+              {{ $i18n( 'vlw-nsfw-modal--cw-subheading' ).text() }}
+            </div>
+            <div id="cw-action-buttons">
+              <button id="cw-confirm" type="button" class="cw-action-button" @click="onClickConfirm">
+                {{ $i18n( 'vlw-nsfw-modal--cw-confirm' ).text() }}
+              </button>
+              <button id="cw-back" type="button" class="cw-action-button" @click="onClickBack">
+                {{ $i18n( 'vlw-nsfw-modal--cw-back' ).text() }}
+              </button>
+            </div>
+            <div id="cw-more-actions">
+              <cdx-checkbox v-model="store.doNotShowMoreWarnings" @update:model-value="onUpdate">
+                {{ $i18n( 'vlw-nsfw-modal--do-not-show' ).text() }}
+              </cdx-checkbox>
+            </div>
+          </div>
+        </div>
+        `,
+        components: { CdxCheckbox },
+        setup: () => ({ store }),
+        methods: {
+          /**
+           * Event handler on clicking the "Yes, I'm over 18" Button
+           */
+          onClickConfirm(): void {
+            showOrHideModal(false);
+
+            // Save user consent to session storage
+            skipIds.push(''+config.wgArticleId);
+            mw.cookie.set( SUPPRESSED_PAGE_IDS_KEY, skipIds.join(',') );
+
+            // Save option to user preferences
+            if (this.store.doNotShowMoreWarnings) {
+              saveUserOptions();  //! unawaited
+            }
+
+            // Clear scroll locking
+            const scrollY = document.body.style.top;
+            document.body.style.position = '';
+            document.body.style.top = '';
+            window.scrollTo(0, parseInt(scrollY || '0') * -1);
+          },
+
+          /**
+           * Event handler on clicking the "Let me go back" Button
+           */
+          onClickBack(): void {
+            if (window.history.length > 1) {
+              window.history.back();
+            }
+            window.close();
+            // Fallback: If window.close() doesn't work (blocked by browser),
+            // redirect to a blank page
+            setTimeout(() => {
+              if (!window.closed) {
+                window.location.href = 'about:blank';
+              }
+            }, 100);
+          }
+        }
+      });
+      document.body.appendChild(container);
+      $app.mount("#"+container.getAttribute('id'));
+    });
 	}
 
 	init();
 	mw.hook('wikipage.content').add(function () {
-		$('#cw-modal').show();
+    showOrHideModal(true);
 		// Lock background scrolling while the modal is open
 		document.body.style.position = 'fixed';
 		document.body.style.top = '-' + window.scrollY + 'px';
 	});
 
-}(mediaWiki, jQuery));
+}(mediaWiki));
